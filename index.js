51dc2084cde33933b76b7426b8bc82b6265d5abf
kdahmoune <karim.dahmoune@ik.me>
2026-07-14 13:19:04 +0200

    add build middleware

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const  cors = require('cors')
 const app = express()
 app.use(cors())
 app.use(express.json())
-//app.use(express.static('build'))
+app.use(express.static('build'))
 
 morgan.token('body', (req, res) => {
   return JSON.stringify(req.body)
